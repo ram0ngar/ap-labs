8fb49c892278b043bf3949f68c7f5ae8b42f3916
Ramon <ram00ngar@gmail.com>
2020-03-25 15:38:55 -0600

    Solve str-lib
    Signed-off-by: Ramon <ram00ngar@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin,char *addition);
+int mystrfind(char *origin,char *substr);
+
+int main(int argc,char **argv) {
+	char *output;
+	int len=mystrlen(argv[1]);
+	printf("Initial length:\t%d\n",len);
+	output = malloc(sizeof(char)*(len+mystrlen(argv[2])));
+	output=mystradd(argv[1],argv[2]);
+	printf("New string:\t%s\n",output);
+	if(mystrfind(output,argv[3])){
+		printf("Substring found:\tYes\n");
+	}
+	else{
+		printf("Substring found:\tNo\n");
+	}
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,54 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+	int count=0;
+	char *p;
+	p=str;
+	while(*p!='\0'){
+		p++;
+		count++;
+	}
+    return count;
 }
-
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int len=0;
+    char *o1,*a1,*o2,*a2, *nWord;
+    o1=o2=origin;
+    a1=a2=addition;
+    len=mystrlen(o1)+mystrlen(a1);
+    char *output = malloc(sizeof(char)*len);
+    nWord=output;
+    while(*o2!='\0'){
+    	*nWord=*o2;
+    	o2++;
+    	nWord++;    
+    }
+    while(*a2!='\0'){
+    	*nWord=*a2;
+    	a2++;
+    	nWord++;
+    }
+    return output;
 }
-
 int mystrfind(char *origin, char *substr){
+    int i;
+    int aux = 0;
+    int originLen = mystrlen(origin);
+    int subLen = mystrlen(substr);
+    for (i = 0; i <= originLen; i++) {
+        if (origin[i] == substr[0] && aux < 1) {
+            aux++;
+        }
+        else if (origin[i] == substr[aux]) {
+            aux++;
+        }
+        else {
+            aux = 0;
+        }
+        if (aux >= subLen) {
+            return 1;
+        }
+    }
     return 0;
 }
